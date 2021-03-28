c8157e2a9f9bfba51727c2d3a474e0f4e4c43b6b
henry440 <440henry@gmail.com>
2021-03-28 03:21:08 +0200

    Vorbereitung für Netzkommunikation

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 #Libarys
 import sys
 #Project
-from res.config import VERSION_KEY
+from res.config import VERSION_KEY, SERVER_IP, SERVER_PORT
 from res.logging import logging
 
 #Global Vars
 TAG = "MAIN"
 log = logging(TAG)
 
-def as_client():
+def as_client(ip, port):
     log.log(f"Starte als Client", 0)
 
-def as_server():
+def as_server(ip, port):
     log.log(f"Starte als Server", 0)
 
 def cli():
     log.log(f"Starte CLI", 0)
     while True:
-        cmd = input()        
+        cmd = input()   
 
 if __name__ == "__main__":
     log.log("Programmstart", 1)    
     log.log(f"Argumente gefunden : {len(sys.argv)}", 0)
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1: #ARG1 .\main.py | ARG2 server/client
+        if len(sys.argv) > 2: #ARG3 IP
+            SERVER_IP = sys.argv[2]
+        if len(sys.argv) > 3: #ARG4 IP
+            SERVER_PORT = int(sys.argv[3])
+
+        log.log(f"Netzwerk Konfig : {SERVER_IP}:{SERVER_PORT}", 0)
         if sys.argv[1] == "server":
-            as_server()
+            as_server(SERVER_IP, SERVER_PORT)
         else:
-            as_client()
+            as_client(SERVER_IP, SERVER_PORT)
     else:
         #Default Clientanwendung
-        as_client()
+        log.log(f"Netzwerk Konfig : {SERVER_IP}:{SERVER_PORT}", 0)
+        as_client(SERVER_IP, SERVER_PORT)
 
     cli()
 else:
--- res/config.py
+++ res/config.py
@@ -6,4 +6,8 @@ VERSION_KEY = "dev"
 LOG_LEVEL = ["DEBUG", "INFO", "WARN", "ERROR", "FATAL"]
 LOG_FILE  = "./res/log.txt"
 LOG_LVL_CONS = 0
-LOG_LVL_FILE = 1
+LOG_LVL_FILE = 0
+
+# Network Config
+SERVER_IP = "192.168.1.10"
+SERVER_PORT = 6006

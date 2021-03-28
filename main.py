72870292da2ead437f5008d0ba42708e6682be6e
henry440 <440henry@gmail.com>
2021-03-28 12:24:09 +0200

    Optimierrung Main
    Main Code wurde optimiert. Besseres Routing der Argumente

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def as_client(ip, port):
 def as_server(ip, port):
     log.log(f"Starte als Server", 0)
 
-def cli():
+def cli(mode):
     log.log(f"Starte CLI", 0)
     while True:
         cmd = input()   
@@ -22,22 +22,21 @@ def cli():
 if __name__ == "__main__":
     log.log("Programmstart", 1)    
     log.log(f"Argumente gefunden : {len(sys.argv)}", 0)
-    if len(sys.argv) > 1: #ARG1 .\main.py | ARG2 server/client
-        if len(sys.argv) > 2: #ARG3 IP
-            SERVER_IP = sys.argv[2]
-        if len(sys.argv) > 3: #ARG4 IP
-            SERVER_PORT = int(sys.argv[3])
 
-        log.log(f"Netzwerk Konfig : {SERVER_IP}:{SERVER_PORT}", 0)
+    mode = "client"
+    if len(sys.argv) > 2: #ARG3 IP
+        SERVER_IP = sys.argv[2]
+    if len(sys.argv) > 3: #ARG4 IP
+        SERVER_PORT = int(sys.argv[3])
+
+    log.log(f"Netzwerk Konfig : {SERVER_IP}:{SERVER_PORT}", 0)
+    if len(sys.argv) > 1:
         if sys.argv[1] == "server":
+            mode = "server"
             as_server(SERVER_IP, SERVER_PORT)
-        else:
-            as_client(SERVER_IP, SERVER_PORT)
     else:
-        #Default Clientanwendung
-        log.log(f"Netzwerk Konfig : {SERVER_IP}:{SERVER_PORT}", 0)
         as_client(SERVER_IP, SERVER_PORT)
 
-    cli()
+    cli(mode)
 else:
     log.log("Kann nicht Importiert werden", 4)

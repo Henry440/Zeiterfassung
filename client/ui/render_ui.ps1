87fb921b5e408d65f75c8fcbd2e1c0bf7e05eeeb
henry440 <440henry@gmail.com>
2021-03-31 12:13:02 +0200

    add mainframe.py: Changes to be committed: 	new file:   mainframe.ui 	modified:   render_ui.ps1
     Changes not staged for commit:
    	deleted:    template/mainframe.ui

--- client/ui/render_ui.ps1
+++ client/ui/render_ui.ps1
@@ -1,5 +1,13 @@
-$Pfad = ".\template\"
-$Files = Get-ChildItem $Pfad
+$Files = Get-ChildItem
 Write-Host $Files
+
+for($i = 0; $i -lt $Files.Length; $i++){
+    $fi = $Files[$i]
+    $f = $fi.Name.Split(".")
+    $out = $f[0] + ".py"
+    if($f[1] -eq "ui"){
+        python -m PyQt5.uic.pyuic -x $fi -o $out
+    }
+}
 
 #python -m PyQt5.uic.pyuic -x mainframe.ui -o mainframe.py
